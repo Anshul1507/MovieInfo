a536700681093ae38a7e090e558045ad542c05cb
Anshul1507 <ag1507anshul@gmail.com>
2020-03-20 20:14:02 +0530

    Initial UI commit & Network States

---
+++ app/src/main/java/com/example/movieinfo/movie_details/SingleMovie.kt
@@ -0,0 +1,13 @@
+package com.example.movieinfo.movie_details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.movieinfo.R
+
+class SingleMovie : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_movie)
+    }
+}
