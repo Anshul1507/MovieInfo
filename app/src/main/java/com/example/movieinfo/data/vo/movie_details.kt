bca93a182fbd5d8a28410727614f3d80e948bc1c
Anshul1507 <ag1507anshul@gmail.com>
2020-03-21 14:51:54 +0530

    updated data classes

---
+++ app/src/main/java/com/example/movieinfo/data/vo/movie_details.kt
@@ -0,0 +1,44 @@
+package com.example.movieinfo.data.vo
+
+
+import com.google.gson.annotations.SerializedName
+
+data class movie_details(
+//    val adult: Boolean,
+//    @SerializedName("backdrop_path")
+//    val backdropPath: String,
+//    @SerializedName("belongs_to_collection")
+//    val belongsToCollection: BelongsToCollection,
+    val budget: Int,
+//    val genres: List<Genre>,
+//    val homepage: String,
+    val id: Int,
+//    @SerializedName("imdb_id")
+//    val imdbId: String,
+//    @SerializedName("original_language")
+//    val originalLanguage: String,
+//    @SerializedName("original_title")
+//    val originalTitle: String,
+    val overview: String,
+    val popularity: Double,
+    @SerializedName("poster_path")
+    val posterPath: String,
+//    @SerializedName("production_companies")
+//    val productionCompanies: List<ProductionCompany>,
+//    @SerializedName("production_countries")
+//    val productionCountries: List<ProductionCountry>,
+    @SerializedName("release_date")
+    val releaseDate: String,
+    val revenue: Long,
+    val runtime: Int,
+//    @SerializedName("spoken_languages")
+//    val spokenLanguages: List<SpokenLanguage>,
+    val status: String,
+    val tagline: String,
+    val title: String,
+    val video: Boolean,
+    @SerializedName("vote_average")
+    val rating: Double
+//    @SerializedName("vote_count")
+//    val voteCount: Int
+)
